c907867445a3108f06978aec4acd060f19f4c0bb
= <=>
2023-01-09 11:49:54 +0530

    9jan commit

--- string/jsstring.js
+++ string/jsstring.js
@@ -4,27 +4,27 @@ var x = 3;
 {
 let x = 2;
 // let x = 3;
-document.getElementById("demo1").innerHTML = x;
+document.getElementById("string1").innerHTML = x;
 }
 
 // {
 // let x = 2;
 // var x = 3;
 // }
-// document.getElementById("demo").innerHTML = x;
+// document.getElementById("string").innerHTML = x;
 let text1 = "Hello";
 let text2 = "World!";
 let text3 = text1.concat(" ","Hi","Hello",text2);
-// document.getElementById("demo").innerHTML = text3;
+// document.getElementById("string").innerHTML = text3;
 let text = "Please visit Microsoft!";
 let newText = text.replace(/MICROSOFT/i, "W3Schools");
-// document.getElementById("demo").innerHTML = newText;
+// document.getElementById("string").innerHTML = newText;
 
 let str = "Hello World!";
 // let part = str.substr(7, 3);
 // let part = str.substring(3, 7);
 let part = str.slice(-11, -2);
-// document.getElementById("demo").innerHTML = part;
+// document.getElementById("string").innerHTML = part;
 
 let header = "Template Literals";
 let tags = ["template literals", "javascript"];
@@ -34,4 +34,4 @@ let html = `<h2>${header}</h2><ul>`;
 for (const x of tags) {
 html += `<li>${x}</li>`;
 }
-document.getElementById("demo").innerHTML = html;
+document.getElementById("string").innerHTML = html;
